8d1a8a8e53dccd205903f307af20f50004957e78
gitcuber369 <arpitchaudhary2003@gmail.com>
2024-10-21 16:49:28 +0530

    [update]:project shocase section left

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { Space_Grotesk } from "next/font/google";
+import "./globals.css";
+
+const font = Space_Grotesk({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Dream Skrin",
+  description:
+    "Grow your business with Dream Skrin, a design,  development and marketing studio.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={font.className}>{children}</body>
+    </html>
+  );
+}
